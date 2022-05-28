aa6ce55b170b10d66c132b3c2554b014342ec7a5
多读书多看报 <1073576293@qq.com>
2022-05-28 21:39:18 +0800

    登录、退出功能

--- src/main.js
+++ src/main.js
@@ -4,10 +4,24 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 将axios包挂载在vue2的实例对象上，这样全局组件就可以使用这个axios
+Vue.prototype.$http = axios
+
+
+// 引入外部图标
+import './assets/fonts/iconfont.css'
+
+// 导入全局样式表
+import './assets/css/global.css'
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
+  
 }).$mount('#app')
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button,Form,FormItem,Input,Message } from 'element-ui'
+
+// 导入消息弹窗
+// 弹窗组件需要通过原型对象挂载全局使用
+Vue.prototype.$message = Message
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
